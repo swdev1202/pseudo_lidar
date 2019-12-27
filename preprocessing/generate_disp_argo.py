c699742784fdb434905ea51e0ab53c7c04e76a8c
Sean Seung Won Lee <swdev1202@gmail.com>
2019-12-26 18:34:19 -0800

    argoverse disparity generation

--- preprocessing/generate_disp_argo.py
+++ preprocessing/generate_disp_argo.py
@@ -8,81 +8,72 @@ from argoverse.data_loading.argoverse_tracking_loader import ArgoverseTrackingLo
 from argoverse.utils.ply_loader import load_ply
 from argoverse.utils.camera_stats import get_image_dims_for_camera as get_dim
 
+# Argoverse Dataset Specific
 NUM_TRAIN = 4
+BASELINE = 0.2986 #page 4 of the paper (https://arxiv.org/pdf/1911.02620.pdf)
+STEREO_IMG_WIDTH = 2464
+STEREO_IMG_HEIGHT = 2056
+LEFT_STEREO_CAM = 'stereo_front_left'
 
-# def generate_dispariy_from_velo(pc_velo, height, width, calib):
-#     pts_2d = calib.project_velo_to_image(pc_velo)
-#     fov_inds = (pts_2d[:, 0] < width - 1) & (pts_2d[:, 0] >= 0) & \
-#                (pts_2d[:, 1] < height - 1) & (pts_2d[:, 1] >= 0)
-#     fov_inds = fov_inds & (pc_velo[:, 0] > 2)
-#     imgfov_pc_velo = pc_velo[fov_inds, :]
-#     imgfov_pts_2d = pts_2d[fov_inds, :]
-#     imgfov_pc_rect = calib.project_velo_to_rect(imgfov_pc_velo)
-#     depth_map = np.zeros((height, width)) - 1
-#     imgfov_pts_2d = np.round(imgfov_pts_2d).astype(int)
-#     for i in range(imgfov_pts_2d.shape[0]):
-#         depth = imgfov_pc_rect[i, 2]
-#         depth_map[int(imgfov_pts_2d[i, 1]), int(imgfov_pts_2d[i, 0])] = depth
-#     baseline = 0.54
+def generate_disparity_from_velo_argo(pc_path, scene_log):
+    pc = load_ply(pc_path) # load point cloud
+    calib = scene_log.get_calibration(LEFT_STEREO_CAM) # load calibration
 
-#     disp_map = (calib.f_u * baseline) / depth_map
-#     return disp_map
+    # uv => nx3 points in image coord + depth
+    uv = calib.project_ego_to_image(pc) # 3D point cloud -> 2d pixel coordinates
+    print(uv.shape)
+    
+    fov_inds = (uv[:, 0] < STEREO_IMG_WIDTH - 1) & (uv[:, 0] >= 0) & \
+               (uv[:, 1] < STEREO_IMG_HEIGHT - 1) & (uv[:, 1] >= 0)
+    fov_inds = fov_inds & (pc[:, 0] > 2)
+    
+    imgfov_pc_velo = pc[fov_inds, :]
+    imgfov_pc_rect = calib.project_ego_to_cam(imgfov_pc_velo)
+    imgfov_pts_2d = uv[fov_inds, :]
+    depth_map = np.zeros((STEREO_IMG_HEIGHT, STEREO_IMG_WIDTH)).astype(int)
+    imgfov_pts_2d = np.round(imgfov_pts_2d).astpye(int)
+    print(imgfov_pts_2d.shape)
+    for i in range(imgfov_pts_2d.shape[0]):
+        depth = imgfov_pc_rect[i, 2]
+        depth_map[int(imgfov_pts_2d[i, 1]), int(imgfov_pts_2d[i, 0])] = depth
+    
+    disp_map = (calib.fu * BASELINE) / depth_map
+    return disp_map
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate Disparity for Argoverse Dataset')
     parser.add_argument('--data_path', type=str, default='~/argoverse-tracking/')
+    parser.add_argument('--train_size', type=int, default=4)
     args = parser.parse_args()
 
     for i in range(NUM_TRAIN+1):
         train_dir = args.data_path + 'train' + str(i) + '/'
+        print('Currently Processing Train ', str(i))
 
         argoverse_loader = ArgoverseTrackingLoader(train_dir)
         print('Total Number of Logs in train', str(i), ': ', len(argoverse_loader))
 
-        cam_left = argoverse_loader.CAMERA_LIST[7] # left pair of the stereo camera
-        cam_right = argoverse_loader.CAMERA_LIST[8] # right pair of the stereo camera
-        (cam_width, cam_height) = get_dim(cam_left)
-        baseline = 0.2986 # page 4 of the paper (https://arxiv.org/pdf/1911.02620.pdf)
-        
-        for log_id in argoverse_loader.log_list:
-            # 
+        for log_index, log_id in enumerate(argoverse_loader.log_list):
+            curr_log = argoverse_loader.get(log_id)
+            curr_dir = train_dir + str(log_id) + '/'
 
+            # create a ground-truth disparity directory
+            disp_dir = curr_dir + 'disparity/'
 
-    assert os.path.isdir(args.data_path)
-    lidar_dir = args.data_path + '/lidar/'
-    calib_file = args.data_path + 'vehicle_calibration_info.json'
-    image_dir = args.data_path + '/stereo_front_left/'
-    disparity_dir = args.data_path + '/disparity/'
+            if not os.path.isdir(disp_dir):
+                os.makedirs(disp_dir)
+            
+            # point to valid file directories to generate ground-truth disparity
+            lidar_dir = curr_dir + 'sync_lidar/'
 
-    assert os.path.isdir(lidar_dir)
-    assert os.path.isfile(calib_file)
-    assert os.path.isdir(image_dir)
+            assert os.path.isdir(lidar_dir)
 
-    if not os.path.isdir(disparity_dir):
-        os.makedirs(disparity_dir)
+            # Argoverse Lidar point clouds are stored as .ply format
+            lidar_files_list = [x for x in os.listdir(lidar_dir) if x[-3:] == 'ply']
+            lidar_files_list = sorted(lidar_files_list)
+            print('Total LiDAR point clouds for this log = ', len(lidar_files_list))
 
-    # Argoverse Lidar point clouds are stored as .ply format
-    lidar_files = [x for x in os.listdir(lidar_dir) if x[-3:] == 'ply']
-    lidar_files = sorted(lidar_files)
-
-    # assert os.path.isfile(args.split_file)
-    # with open(args.split_file, 'r') as f:
-    #     file_names = [x.strip() for x in f.readlines()]
-
-    for fn in lidar_files:
-        predix = fn[:-4]
-        #if predix not in file_names:
-        #    continue
-
-        calib_file = '{}/{}.txt'.format(calib_dir, predix)
-        calib = kitti_util.Calibration(calib_file)
-        # load point cloud
-        lidar = np.fromfile(lidar_dir + '/' + fn, dtype=np.float32).reshape((-1, 4))[:, :3]
-        image_file = '{}/{}.png'.format(image_dir, predix)
-        image = ssc.imread(image_file)
-        height, width = image.shape[:2]
-        disp = generate_dispariy_from_velo(lidar, height, width, calib)
-        np.save(disparity_dir + '/' + predix, disp)
-        print('Finish Disparity {}'.format(predix))
-
+            for fn in lidar_files_list:
+                lidar_file_path = lidar_dir + fn
+                grnd_truth_disp = generate_disparity_from_velo_argo(lidar_file_path, curr_log)
