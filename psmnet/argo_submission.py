d13fe6c84ab7efaf8609925209e76f062485ba05
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-21 15:53:25 -0800

    argoverse fullsize option

--- psmnet/argo_submission.py
+++ psmnet/argo_submission.py
@@ -38,7 +38,8 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--save_path', type=str, default='finetune_1000', metavar='S',
                     help='path to save the predict')
-parser.add_argument('--save_figure', action='store_true', help='if true, save the numpy file, not the png file')
+parser.add_argument('--save_figure', action='store_true', help='if true, save the png file, not the npy file')
+parser.add_argument('--fullsize', action='store_true', help='if true, use a fullsize image')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -49,8 +50,7 @@ if args.cuda:
 if args.KITTI == '2015':
    from dataloader import KITTI_submission_loader as DA
 else:
-   from dataloader import KITTI_submission_loader2012 as DA  
-
+   from dataloader import KITTI_submission_loader2012 as DA
 
 test_left_img, test_right_img = DA.dataloader(args.datapath)
 
@@ -67,6 +67,13 @@ model.cuda()
 if args.loadmodel is not None:
     state_dict = torch.load(args.loadmodel)
     model.load_state_dict(state_dict['state_dict'])
+
+if(args.fullsize):
+    top_pad_const = 2064
+    left_pad_const = 2464
+else:
+    top_pad_const = 544
+    left_pad_const = 1248
 
 print('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
 
@@ -98,9 +105,10 @@ def main():
         imgL_o = (skimage.io.imread(test_left_img[inx]).astype('float32'))
         imgR_o = (skimage.io.imread(test_right_img[inx]).astype('float32'))
 
-        # downsample to (H/4 , W/2)
-        imgL_o = skimage.transform.downscale_local_mean(imgL_o, (4,2,1))
-        imgR_o = skimage.transform.downscale_local_mean(imgR_o, (4,2,1))
+        if(args.fullsize == False):
+            # downsample to (H/4 , W/2)
+            imgL_o = skimage.transform.downscale_local_mean(imgL_o, (4,2,1))
+            imgR_o = skimage.transform.downscale_local_mean(imgR_o, (4,2,1))
 
         # process the image
         imgL = processed(imgL_o).numpy()
@@ -110,9 +118,8 @@ def main():
         imgL = np.reshape(imgL,[1,3,imgL.shape[1],imgL.shape[2]]) #(B, C, H, W)
         imgR = np.reshape(imgR,[1,3,imgR.shape[1],imgR.shape[2]])
 
-        # pad to (544, 1248)
-        top_pad = 544-imgL.shape[2]
-        left_pad = 1248-imgL.shape[3]
+        top_pad = top_pad_const-imgL.shape[2]
+        left_pad = left_pad_const-imgL.shape[3]
         imgL = np.lib.pad(imgL,((0,0),(0,0),(top_pad,0),(0,left_pad)),mode='constant',constant_values=0)
         imgR = np.lib.pad(imgR,((0,0),(0,0),(top_pad,0),(0,left_pad)),mode='constant',constant_values=0)
 
@@ -120,8 +127,8 @@ def main():
         pred_disp = test(imgL,imgR)
         print('time = %.2f' %(time.time() - start_time))
 
-        top_pad   = 544-imgL_o.shape[0]
-        left_pad  = 1248-imgL_o.shape[1]
+        top_pad   = top_pad_const-imgL_o.shape[0]
+        left_pad  = left_pad_const-imgL_o.shape[1]
         img = pred_disp[top_pad:,:-left_pad]
         print(test_left_img[inx].split('/')[-1])
         if args.save_figure:
