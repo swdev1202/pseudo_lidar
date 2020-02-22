46053b4394e18208f56b7114a97d9972793a279e
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-22 00:20:52 -0800

    fixing errors on load model

--- psmnet/argo_submission.py
+++ psmnet/argo_submission.py
@@ -81,13 +81,12 @@ if args.loadmodel is not None:
 
     # getting rid of module. in state dict
     substring = 'module.'
-    state_dict_tmp = OrderedDict()
-    for k in state_dict:
+    state_dict_converted = OrderedDict()
+    for k in state_dict['state_dict']:
         new_k = k[len(substring):] if k.startswith(substring) else k
-        state_dict_tmp[new_k] = state_dict[k]
-    state_dict = state_dict_tmp
+        state_dict_converted[new_k] = state_dict[k]
 
-    model.load_state_dict(state_dict['state_dict'])
+    model.load_state_dict(state_dict_converted)
 
 print('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
 
